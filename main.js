01807488d302f9d9523a2af599a0e252a63c2032
AbhirajGOAT <84024511+AbhirajGOAT@users.noreply.github.com>
2023-01-06 16:46:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX = 0;
+noseY = 0;
+
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.hide();
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background('#FF5C77');
+    image(video, 50, 50, 450, 400);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+
+    }
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Initialized');
+}
+
